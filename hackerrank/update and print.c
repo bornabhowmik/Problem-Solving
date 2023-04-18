3729f247fbb1b7b912500dad49d389260599ad4a
bornabhowmik <bornabhowmik4@gmail.com>
2023-04-19 02:48:18 +0600

    update and print code added

--- hackerrank/update and print.c
+++ hackerrank/update and print.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int N, X, V, i;
+    int N, X, V;
     scanf("%d", &N);
 
     int arr[N];
-    for(int i=0; i<N; i++);{ 
+    for(int i=0; i<N; i++){ 
     scanf("%d", &arr[i]);
     }
 
     scanf("%d %d", &X, &V);
-    arr[X-1]=V;
+    arr[X]=V;
 
     for(int i=N-1; i>=0; i--){
     printf("%d ", arr[i]);
@@ -18,5 +18,4 @@ int main()
     printf("\n");
 
     return 0;
-
 }
