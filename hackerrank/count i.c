764484cb5f80598dc2674191ea7251171f2ec05c
bornabhowmik <bornabhowmik4@gmail.com>
2023-06-29 23:18:02 +0600

    even and odd.c code added

---
+++ hackerrank/count i.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int ar[n];
+    sca
+    for(int i=1; i<=n; i++ )
+    {
+        if(i%2==0)
+        {
+            printf("%d-even\n",i);
+        }
+        else
+        {
+            printf("%d-odd\n",i);
+        }
+    }
+    
+    return 0;
+}
