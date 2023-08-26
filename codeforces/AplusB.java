543eb664d85afc43fed73ed13b14c3ac88756e83
bornabhowmik <bornabhowmik4@gmail.com>
2023-08-26 19:44:24 +0600

    even_odds.java code added

--- codeforces/AplusB.java
+++ codeforces/AplusB.java
@@ -1,16 +1,18 @@
 import java.util.*;
 public class AplusB {
     public static void main(String args[]) {
+
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        int a,b;
+        sc.nextLine();
 
-        while(t--<0){
-            a=sc.nextInt();
-            b=sc.nextInt();
-            if(a>0 && b>0 && a<=9 && b<=9){
-                System.out.println(a+b);
-            }
+        for(int i=0; i<t; i++){
+            String stringLine=sc.nextLine();
+            int result= Character.getNumericValue(stringLine.charAt(0)) + Character.getNumericValue(stringLine.charAt(2));
+
+            System.out.println(result);
         }
+
+        sc.close();
     }
 }
