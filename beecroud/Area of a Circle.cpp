c988adf1511672f56dea665ff7cd486a45bcbd4b
bornabhowmik <bornabhowmik4@gmail.com>
2023-10-11 04:03:48 +0600

    Area of a Circle.cpp code added

---
+++ beecroud/Area of a Circle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    double R, A;
+    cin>>R;
+    
+    const double pi=3.14159;
+
+    A=pi * R * R;
+
+    cout<<"A = "<< fixed << setprecision(4) << A << endl;
+
+    return 0;
+}
