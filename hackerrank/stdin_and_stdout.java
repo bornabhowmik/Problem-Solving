07d8e07d5f281fd163f1e417fc169e773f43de04
bornabhowmik <bornabhowmik4@gmail.com>
2023-11-09 22:24:10 +0600

    stdin_and_stdout.java code added

--- hackerrank/stdin_and_stdout.java
+++ hackerrank/stdin_and_stdout.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class stdin_and_stdout {
     
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            int a = scanner.nextInt();
+            System.out.println(a);
+        }
+
+        scanner.close();
+    }
 }
