41c3b3df76907c25c654875830595bd6303061c6
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-21 23:47:47 +0600

    input14.cpp code added

---
+++ codechef/input14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,z;
+    cin>>t;
+    while (t--){
+
+        cin>>x>>z;
+        
+        if (x>=z){
+            cout<<"YES"<<endl;
+        // }
+        //  else if(x==z){
+        //     cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }    
+}
+
+	return 0;
+    
+}
