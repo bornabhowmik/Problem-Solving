422e81ad8ad0fae9110447733ee0239833f50a5a
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-21 23:36:05 +0600

    input13.cpp code added

---
+++ codechef/input13.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int X,Y;
+	cin>>X>>Y;
+	
+	if(X<Y){
+	    cout<<"YES"<<endl;
+	    
+	}
+	else if(Y=X){
+	    cout<<"NO"<<endl;
+	}
+	else{
+	    
+	    cout<<"NO"<<endl;
+	}
+	    
+	return 0;
+}
