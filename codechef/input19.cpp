c2d788540fb5ebf697f9282580783c8a8d4effad
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-24 23:36:52 +0600

    input19.cpp code added

---
+++ codechef/input19.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+	
+	if(x<y){
+        printf("YES");
+    }else{
+        printf("NO");
+    }
+	return 0;
+}
