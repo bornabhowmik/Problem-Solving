b6b850cec7ebe8c803cd6e1fe5fd61e2b964d4fb
bornabhowmik <bornabhowmik4@gmail.com>
2024-01-27 23:07:55 +0600

    input21.cpp code added

---
+++ codechef/input21.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    
+    while (t--) {
+        int x;
+        scanf("%d", &x);
+        
+        int a = x*10;
+        
+        printf("%d/n", a);
+        
+        return 0;
+    }
+}
