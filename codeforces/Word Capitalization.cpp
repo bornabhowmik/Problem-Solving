33be474d4aa29116cc7619eff0bff754bb7736da
bornabhowmik <bornabhowmik4@gmail.com>
2024-05-24 23:30:10 +0600

    all code added

---
+++ codeforces/Word Capitalization.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string word;
+    
+    cin >> word;
+    
+
+    if(word[0]!= toupper(word[0])){
+        word[0]=toupper(word[0]);
+    }
+
+    cout << word << endl;
+
+    return 0;
+}
